4cac70c3af4c26e2bc71b53c20d05d907a748554
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-10-29 13:49:57 -0400

    completed oojs

--- oojs.js
+++ oojs.js
@@ -5,7 +5,15 @@
 // - a `members` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
-
+class Group {
+  constructor(name){
+    this.name = name
+    this.members = []
+  }
+  addMember(str){
+    this.members.push(str)
+  }
+}
 
 
 
@@ -13,8 +21,8 @@
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
-
-
+var classmates = new Group ("Jesse")
+classmates.addMember("Jesse")
 
 
 
