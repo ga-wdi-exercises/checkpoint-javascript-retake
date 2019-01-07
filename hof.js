0eb01126076486422b99649264313b3bc5ce1106
Jessica Lee <jsurkilee@gmail.com>
2019-01-07 14:32:45 -0500

    retake

--- hof.js
+++ hof.js
@@ -21,9 +21,17 @@ var people = [
 // called `peopleAges`.
 // Type your solution immediately below this line:
 
-
+var peopleAges = people.map((person) => {
+  return person.age
+});
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are WDI students. Assign the returned array
 // to a variable called `wdiStudents`.
 // Type your solution immediately below this line:
+
+var wdiStudents = people.filter((person => {
+    if (person.course == 'WDI') {
+        return person;
+    }
+}));
--- oojs.js
+++ oojs.js
@@ -6,16 +6,25 @@
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
 
+class Group {
+	constructor(name) {
 
+		this.name = name
+		this.members = [];
 
+	}
 
+	addMember(person) {
+		return this.members.push(person)
+	}
+}
 
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
 
-
-
+var classmates = new Group("WDI")
+// classmates.person(Jessica)
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
