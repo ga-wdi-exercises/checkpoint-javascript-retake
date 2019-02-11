0ff884bf8a674d5d20c87e20c9175996805b7289
Joshua Vance <skinsfandc0612@icloud.com>
2019-02-11 14:30:47 -0500

    hopefully I passed 2

--- oojs.js
+++ oojs.js
@@ -6,21 +6,18 @@
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
 
-
-
-
+constructor = class {
+  name = "";
+  members = [];
+};
 
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
 
-
-
-
-
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Group !== 'undefined') {
+if (typeof Group !== "undefined") {
   module.exports = {
     Group
-  }
+  };
 }
