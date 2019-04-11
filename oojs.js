f66e92ed7f13c2ecfffd2e397d0cbf5b99721a02
Amlak Befekadu <amlakbarkon@gmail.com>
2019-04-11 14:36:23 -0400

    adding to pull request

--- oojs.js
+++ oojs.js
@@ -5,6 +5,16 @@
 // - a `members` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
+class Group {
+  constructor(name, members) {
+    this.name = name
+    this.memebers = members
+    }
+    addMember(){
+
+    }
+}
+
 
 
 
@@ -14,7 +24,8 @@
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
 
-
+var classmates = new Group("amlak", "yes");
+classmates.addMember();
 
 
 
