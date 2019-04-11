25ef47b061a21dbb555ea044be374d88f8166aaa
Frederick Banks <frederickbanks@icloud.com>
2019-04-11 14:34:07 -0400

    final commit

--- fundamentals.js
+++ fundamentals.js
@@ -3,17 +3,21 @@
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
 
+var colors = ["red","blue","green"];
+
 
 
 // #2: Access the last item in the array and assign to a variable called `lastColor`.
 // Type your solution immediately below this line:
+var lastColor = colors[2]
+
 
 
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
 
-
+var favoriteColors = [];
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
@@ -22,6 +26,11 @@
 
 // #5: Create an object literal called `student` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var student = {
+    name: 'Fred',
+    age: '26',
+    location: 'Virginia'
+};
 
 
 
@@ -29,3 +38,4 @@
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+student['attends-office-hours'] = true;
--- oojs.js
+++ oojs.js
@@ -6,6 +6,17 @@
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
 
+class Group {
+  constructor(name) {
+    this.name = name
+    this.members = []
+  }
+  addMember() {
+    members.push(object);
+  } 
+};
+
+
 
 
 
@@ -13,6 +24,8 @@
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
+
+var classmates = new Group('Fred')
 
 
 
